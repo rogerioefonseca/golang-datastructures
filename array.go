beecec8ad216fe1b8f303466ae727938695e1be7
rogerio <r.fonseca@serrala.com>
2022-08-13 10:46:13 +0200

    Implementing linkedlist

--- array.go
+++ array.go
@@ -2,17 +2,20 @@ package main
 
 import "fmt"
 
-type array struct {
-	value string
-	index int
-}
+func addToBeggining(v int, arr []int) []int {
+	previous := arr[0]
+	fmt.Println(arr[0])
+	arr[0] = v
+	for i := 1; i <= len(arr)+1; i++ {
+		arr[i] = previous
+		previous = arr[i]
+	}
 
-func pop(arr []array) {
-	lastIndex := len(arr) - 1
-	fmt.Println(lastIndex)
+	return arr
 }
 
 func main() {
-	arr := []array{}
-	arr = append(arr, array{value: "1", index: 0})
+	arr := []int{1, 2, 3, 4}
+	arr[4] = 300
+	fmt.Println(addToBeggining(0, arr))
 }
--- linkedList.go
+++ linkedList.go
@@ -18,7 +18,6 @@ func (l *LinkedList) prepend(n *Node) {
 	n.next = l.head
 	l.head.previous = n
 	l.head = n
-
 }
 
 //Append into the end of the linked list
@@ -26,6 +25,21 @@ func (l *LinkedList) add(n *Node) {
 	l.tail.next = n
 	n.previous = l.tail
 	l.tail = n
+}
+
+func (l *LinkedList) insert(n *Node, index int) {
+	actualNode := l.head
+	actualIndex := 0
+	for actualNode.next != nil {
+		if actualIndex == index {
+			n.previous = actualNode.previous
+			n.next = actualNode
+			n.previous.next = n
+			return
+		}
+		actualNode = actualNode.next
+		actualIndex++
+	}
 }
 
 func newLinkedList(node *Node) LinkedList {
@@ -36,6 +50,43 @@ func newLinkedList(node *Node) LinkedList {
 	return l
 }
 
+func (l *LinkedList) remove(nIndex int) {
+	actualNode := l.head
+	index := 0
+	for actualNode != nil {
+		if (nIndex - 1) == index {
+			actualNode.next = actualNode.next.next
+			return
+		}
+
+		actualNode = actualNode.next
+		index++
+	}
+}
+
+func printAll(l LinkedList) {
+	actualNode := l.head
+	var arr []string
+	for actualNode != nil {
+		arr = append(arr, actualNode.data)
+		actualNode = actualNode.next
+	}
+
+	fmt.Println(arr)
+
+}
+
+func (l *LinkedList) reverse() {
+	actualNode := l.tail
+	l.head = actualNode
+	actualNode.previous = nil
+	for actualNode != nil {
+		actualNode.next = actualNode.previous
+		actualNode = actualNode.previous
+	}
+	fmt.Println(l.head)
+}
+
 func main() {
 	n1 := &Node{data: "111"}
 	n2 := &Node{data: "222"}
@@ -43,25 +94,23 @@ func main() {
 	n4 := &Node{data: "444"}
 	n5 := &Node{data: "555"}
 	n6 := &Node{data: "666"}
+	n7 := &Node{data: "AAAA"}
 	l := newLinkedList(n1)
 	l.prepend(n2)
 	l.prepend(n3)
 	l.prepend(n4)
-	l.add(n5)
-	l.add(n6)
-
-	actualNode := l.head
-	fmt.Println(l.head.next)
-	for actualNode.next != nil {
-		fmt.Printf("%s\n", actualNode.data)
-		actualNode = actualNode.next
-	}
-
-	lastNode := l.tail
-
-	for lastNode != nil {
-		fmt.Println(lastNode)
-		lastNode = lastNode.previous
-	}
-
+	l.prepend(n5)
+	l.prepend(n6)
+	l.insert(n7, 2)
+	printAll(l)
+	l.remove(3)
+	printAll(l)
+	l.reverse()
+	printAll(l)
+	//	lastNode := l.tail
+	//
+	//	for lastNode != nil {
+	//		//fmt.Println(lastNode)
+	//		lastNode = lastNode.previous
+	//	}
 }
